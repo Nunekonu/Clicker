035bbe1e4c5ebdd61b4feb4a3d0bf1c20d87b57e
PeanutBouter <cedric.cabral.robert@gmail.com>
2025-04-10 19:04:00 -0400

    upgrade fonctionne exponentiellement

--- Clicker-Game-main/Clicker-Game-main/scenes/clicker_prototype/clicker_prototype.gd
+++ Clicker-Game-main/Clicker-Game-main/scenes/clicker_prototype/clicker_prototype.gd
@@ -1,26 +1,23 @@
 class_name ClickerPrototype
 extends Control
 
-
 @export
 
-
-
 var power : int = 0
-
-
 var gold : int = 0
-var buttonPressed : bool
+
 
 func _ready() -> void:
 	update_label()
 	update_label1()
-
+	update_label2()
 
 func update_label() -> void:
 	$Label.text = str(gold)
-	
 
+
+func update_label2() -> void:
+	$Label2.text = str(10 + (power ** 2))
 
 
 func increase_clicks() -> void:
@@ -38,9 +35,18 @@ func update_label1() -> void:
 
 
 func _on_upgrade_1_pressed() -> void:
-	increase_clicks1()
+	if gold >= 10 + power ** 2:
+		increase_clicks1()
+		update_label2()
 
 
 func increase_clicks1() -> void:
+	gold -= 10 + power ** 2
 	power += 1
 	update_label1()
+	update_label()
+
+
+func _on_button_3_pressed() -> void:
+	gold += 1
+	update_label()
